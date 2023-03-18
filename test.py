fe86baf7ee8d64256afb262b8c271029b3ce16a6
Roberto Bernardelli <roberto@bernardelli.net>
2023-03-18 11:14:31 +0100

    reverted to 645888b

--- test.py
+++ test.py
@@ -3,11 +3,9 @@ from utils import *
 
 intersection1 = Intersection()
 
-intersection1.spawn_some_cars(10)
-
 for timestep in range(1000):
-    if timestep % 10 == 0:
+    if timestep % 100 == 0:
         intersection1.switch_stoplights()
     intersection1.step()
     intersection1.render()
-    time.sleep(0.5)
+    # time.sleep(0.005)
