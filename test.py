9e4cee4e08638eea977940d3a6d59eb83b64c97a
Emanuele Coccia <emanuele.coccia@studbocconi.it>
2023-03-19 10:47:47 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,11 @@
 from environment import *
-from utils import *
 
 intersection1 = Environment()
 
-for timestep in range(1000):
+for timestep in range(10000):
     if timestep % 100 == 0:
         intersection1.step(switch_stoplights = True)
     else:
         intersection1.step(switch_stoplights = False)
     intersection1.render()
-    time.sleep(0.0001)
+    #time.sleep(0.0001)
