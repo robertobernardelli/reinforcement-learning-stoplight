3256acead966c1d4b6e7dd2a3a4cffb0cf1def3d
Roberto Bernardelli <roberto@bernardelli.net>
2023-05-01 10:07:08 +0200

    update

--- test.py
+++ test.py
@@ -1,21 +1,9 @@
 from environment import *
-<<<<<<< Updated upstream
-from utils import *
-=======
 import datetime
 import time
->>>>>>> Stashed changes
 
 intersection1 = Intersection()
 
-<<<<<<< Updated upstream
-for timestep in range(1000):
-    if timestep % 100 == 0:
-        intersection1.switch_stoplights()
-    intersection1.step()
-    intersection1.render()
-    # time.sleep(0.005)
-=======
 render = True
 fps_limiter = True
 
@@ -30,4 +18,3 @@ for timestep in range(10000):
                 time.sleep(0.01)
             dt = datetime.datetime.now()
         intersection1.render()
->>>>>>> Stashed changes
