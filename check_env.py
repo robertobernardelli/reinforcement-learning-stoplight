da491dcb424f801ec3ece3b033341451b5b60726
Roberto Bernardelli <roberto@bernardelli.net>
2023-05-15 21:22:07 +0200

    deleted temp files

--- check_env.py
+++
@@ -1,6 +0,0 @@
-from stable_baselines3.common.env_checker import check_env
-from gym_env import StoplightEnv
-
-env = StoplightEnv(debug=True, render=False)
-# It will check your custom environment and output additional warnings if needed
-check_env(env)
