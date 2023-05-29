579fe8658bf4843fa4a90696f9bfe1ee6eca2da6
Emanuele Coccia <emanuele.coccia@studbocconi.it>
2023-05-29 18:21:01 +0200

    comments

--- car.py
+++ car.py
@@ -13,12 +13,12 @@ class Car:
         self.list_index = 0 #which node of the path connects the current linked list of cars
         self.pos_index = 1 #node of the path the car is referring to as next_node
         self.next_node = path[self.pos_index]
-        self.next_node.queue.add(self)
+        self.next_node.queue.add(self) #when initializing, add the car to the queue of the next node
         self.distance = self.determine_distance()
         self.direction = self.get_direction()
-        self.time = 0
+        self.time = 0 #time spent on this earth
         
-        #we assume that the speed of the car is normally distributed and has 5% standard deviation
+        #we assume that the speed and acceleration of the car are normally distributed and have 5% standard deviation
         self.speed = np.random.normal(SPEED, SPEED/20) 
         self.max_speed = np.random.normal(MAX_SPEED, MAX_SPEED/20)
         self.acceleration = np.random.normal(ACCELERATION, ACCELERATION/20)
@@ -27,6 +27,7 @@ class Car:
         self.color = (255, 255, 255)
 
     def determine_distance(self):
+        #if the next node is a stoplight and it is red, or if the next node is a yield node and it is occupied
         if (isinstance(self.next_node, nodes.Stoplight) and self.next_node.red) or \
             (isinstance(self.next_node, nodes.YieldNode) and self.next_node.is_occupied()):
             if self.next == None:
@@ -34,23 +35,27 @@ class Car:
             else:
                 return min(math.dist(self.pos, self.next.pos), 
                         math.dist(self.pos, self.next_node.pos))
+        #else, if there is no car ahead
         elif self.next == None:
             return np.inf
+        #else, if there is a car ahead
         else:
             return math.dist(self.pos, self.next.pos)
         
     def get_direction(self):
         direction_not_norm = self.next_node.pos - self.pos
+        #normalize the direction vector
         return direction_not_norm/np.sqrt(np.sum((direction_not_norm)**2))
         
     def angle_between_vectors(self, a, b):
+        #returns the cosine of the angle between two vectors
         return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
 
     def step(self):
         self.time += 1
         new_distance = self.determine_distance()
         
-        #new stoplight
+        #new node
         if self.angle_between_vectors(self.next_node.pos - self.pos, self.direction) < 0.7:
             self.next_node.queue.remove(self)
             self.pos_index += 1
@@ -58,23 +63,25 @@ class Car:
             self.next_node.queue.add(self)
             self.direction = self.get_direction()
 
-            #if the new stoplight is a ListNode
+            #if the new node is a ListNode, remove the car from the old list and add it to the new list
             if isinstance(self.next_node, nodes.ListNode):
                 self.path[self.list_index].car_list.remove_car(self)
                 self.list_index = self.pos_index
                 self.path[self.list_index].car_list.front_append(self)
 
-        #kill
+        #kill, remove from list and return time spent on this earth
         if isinstance(self.next_node, nodes.FinalNode):
             self.path[self.list_index].car_list.remove_car(self)
             self.list_index = self.pos_index
             return self.time
 
+        #accelerate/decelerate
         if new_distance < self.lookahead + SAFETY_DISTANCE:
             self.speed = max(self.speed/2, 0)
         else:
             self.speed = min(self.speed+self.acceleration, self.max_speed)
             
+        #updating other parameters
         self.distance = new_distance
         self.lookahead = self.speed * LOOKAHEAD
         self.pos = self.pos + (self.speed*self.direction)
